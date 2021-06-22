4f05ec422118339c95081ec553de76e288a9934f
eventhorizn <41446698+eventhorizn@users.noreply.github.com>
2021-06-21 21:50:29 -0400

    manual form validation

--- 07-feedback_forms/reviews/views.py
+++ 07-feedback_forms/reviews/views.py
@@ -7,11 +7,19 @@ from django.shortcuts import redirect, render
 def review(request):
     if request.method == 'POST':
         entered_username = request.POST['username']
+
+        if entered_username == '' and len(entered_username) >= 100:
+            return render(request, "reviews/review.html", {
+                'has_error': True
+            })
+
         print(entered_username)
 
         return HttpResponseRedirect('/thank-you')
 
-    return render(request, "reviews/review.html")
+    return render(request, "reviews/review.html", {
+        'has_error': False
+    })
 
 
 def thank_you(request):
