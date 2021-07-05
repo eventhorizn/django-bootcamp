836a1f91c6f5c07bf45ea499d24f786892accd68
eventhorizn <41446698+eventhorizn@users.noreply.github.com>
2021-07-04 21:33:47 -0400

    adding and styling comment form

--- 11-my_site_forms_files_sessions/blog/views.py
+++ 11-my_site_forms_files_sessions/blog/views.py
@@ -4,6 +4,7 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.base import TemplateView
 
 from .models import Post
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -34,4 +35,5 @@ class SinglePostView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['post_tags'] = self.object.tags.all()
+        context['comment_form'] = CommentForm()
         return context
